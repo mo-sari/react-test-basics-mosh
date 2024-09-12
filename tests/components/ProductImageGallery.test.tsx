27dc6876d33ff9c59e15eb7a100d275c086ee65d
mohsen ansari <mohsen.ansari.bim@gmail.com>
2024-09-12 20:52:31 +0330

    inspecting the urls of all images

--- tests/components/ProductImageGallery.test.tsx
+++ tests/components/ProductImageGallery.test.tsx
@@ -9,12 +9,16 @@ describe('ProductImageGallery', () => {
         
         expect(container).toBeEmptyDOMElement();
     });
-    // it('must have an empty page if no image url is passed', () => {
-    //     const imgurls = ['url1', 'url2']
-    //     render(<ProductImageGallery imageUrls={imgurls}/>)
+    it('must render a list of images if is passed', () => {
+        const imgurls = ['url1', 'url2']
+        render(<ProductImageGallery imageUrls={imgurls}/>)
         
-    //     imgurls.forEach(ulr =>{
-            
-    //     })
-    // });
+        // images have no text value, so we can't iterate over them 
+        // and get single elements one by one
+        const images = screen.getAllByRole('img');
+        imgurls.map((url, inx)=>{
+            expect(images[inx]).toHaveAttribute('src', url);
+        })
+        expect(images).toHaveLength(2);
+    });
 })
